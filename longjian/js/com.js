dba5f5b45cfa2af50a17b87b0c80e9509745b709
anchen1204 <anchen1204@gmail.com>
2014-03-07 14:51:20 +0800

    3.7

--- longjian/js/com.js
+++ longjian/js/com.js
@@ -1,6 +1,18 @@
-(function($){
+/*(function($){
     nie.use(['ui.tab'],function(){
         $.tab('#tab .tabs li','#tab .tabSCon');
     });
 })(jQuery);
-/*nie.config.copyRight.setWhite();*/
+	nie.config.copyRight.setWhite();*/
+
+jQuery(document).ready(function($) {
+	var $li=$("#tab ul li");
+	$li.mouseover(function(){
+		$(this).addClass('current')
+		.siblings().removeClass('current');
+		var index=$li.index(this);
+		var $tabCon=$("#tab .tabCon  .tabSCon")
+			.eq(index).addClass('current')
+			.siblings().removeClass('current');
+	});
+});
