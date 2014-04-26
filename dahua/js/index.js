d3d64862ff04b7a48e0a889b43b7e8a2e66e6d66
anchen1204 <anchen1204@gmail.com>
2014-04-26 16:44:55 +0800

    change more

--- dahua/js/index.js
+++ dahua/js/index.js
@@ -1,27 +1,34 @@
 
+window.onload=function(){
+	var oDiv=document.getElementById('div1');
+	oDiv.onmouseover=function(){
+		startMove(0);
+	}
+	oDiv.onmouseout=function(){
+		startMove(-160);
+	}
+}
+var timer=null;
+ function startMove(iTarget){
+ 	var oDiv=document.getElementById('div1');
+ 	clearInterval(timer);
+ 	timer=setInterval(function(){
+ 		var iSpeed=0;
+ 		if(oDiv.offsetLeft>iTarget){
+ 			iSpeed=-10;
+ 		}else{
+ 			iSpeed=10;
+ 		}
+ 		if(oDiv.offsetLeft==iTarget){
+ 			clearInterval(timer);
+ 		}else{
+ 			oDiv.style.left=oDiv.offsetLeft+iSpeed+'px';
+ 		}
+ 	},30)
+ }
 
-$(function(){ 
-$(".but").click(function(){ 
-$("#faqbg").css({display:"block",height:$(document).height()}); 
-var yscroll =document.documentElement.scrollTop; 
-$("#faqdiv").css("top","100px"); 
-$("#faqdiv").css("display","block"); 
-document.documentElement.scrollTop=0; 
-}); 
-$(".close").click(function(){ 
-$("#faqbg").css("display","none"); 
-$("#faqdiv").css("display","none"); 
-}); 
-}) 
-$(function(){
-	nie.use(["util.share","util.fixPNG"],function(){
-		$.share.appendTo("#rShare",{title:""});	
-		var share2=nie.util.share({
-　			fat:".rWShare",
-　			type:2,
-　			title:"《大话西游2》资料片灵宝降魔开测发大礼啦！答题有奖，iPad mini、鼠标垫，精美陶瓷杯等你来拿。",
-			img:"/images/fenxiang.jpg"
-		});
-	});
-});
-nie.config.copyRight.setWhite();
+
+
+
+
+
